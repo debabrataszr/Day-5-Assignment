b7f2a457f50ec1537ab094a867accef4a848ce12
debabrata-97 <debabrataszr@gmail.com>
2023-03-02 21:52:26 +0530

    VowelConsonant

---
+++ src/com/bridgelabz/VowelConsonant.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        char ch = 'b';
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println(ch + " is a vowel");
+        } else {
+            System.out.println(ch + " is a consonant");
+        }
+    }
+}
+
