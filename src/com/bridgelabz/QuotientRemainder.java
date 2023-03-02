2912c233a19f8e84754a5351b3a25daebd5869e9
debabrata-97 <debabrataszr@gmail.com>
2023-03-02 21:39:47 +0530

    QuotientRemainder

---
+++ src/com/bridgelabz/QuotientRemainder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        int dividend = 100, divisor = 5;
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}
